f555e19b2e56aa7d9cbac2cd4877e639326584ae
조현석 <hsjo0227@gmail.com>
2024-12-24 16:41:46 +0900

    프로젝트 생성

---
+++ paginationPractice/src/main/java/com/ssafy/paginationpractice/PaginationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.paginationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationPracticeApplication.class, args);
+    }
+
+}
---
+++ paginationPractice/src/test/java/com/ssafy/paginationpractice/PaginationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.paginationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
